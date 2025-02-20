12494efc55622475af2a47aa11d7a2287cdb90e1
Myo Min Hein <myominhein.irrahub@gmail.com>
2025-02-21 00:50:03 +0700

    update config for flutter bloc plugin

--- lua/plugins/flutter-bloc.lua
+++ lua/plugins/flutter-bloc.lua
@@ -1,3 +1,11 @@
 return {
   "wa11breaker/flutter-bloc.nvim",
+  dependencies = {
+    "nvimtools/none-ls.nvim", -- Required for code actions
+  },
+  opts = {
+    bloc_type = "equatable", -- Choose from: 'default', 'equatable', 'freezed'
+    use_sealed_classes = true,
+    enable_code_actions = true,
+  },
 }
