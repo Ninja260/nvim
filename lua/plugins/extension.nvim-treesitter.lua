5c2ab12680c80b75d681559536a66e62c9265bce
Myo Min Hein <myominhein.irrahub@gmail.com>
2025-12-17 01:43:53 +0700

    install dart treesitter

---
+++ lua/plugins/extension.nvim-treesitter.lua
@@ -0,0 +1,36 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    -- LazyVim config for treesitter
+    indent = { enable = true }, ---@type lazyvim.TSFeat
+    highlight = { enable = true }, ---@type lazyvim.TSFeat
+    folds = { enable = true }, ---@type lazyvim.TSFeat
+    ensure_installed = {
+      "bash",
+      "c",
+      "dart",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+  },
+}
