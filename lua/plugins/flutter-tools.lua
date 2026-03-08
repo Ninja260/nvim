353867c14458e6f0ac0e93b2942e09f8a50257d8
Myo Min Hein <myominhein.irrahub@gmail.com>
2026-03-08 21:25:35 +0700

    update flutter lsp keybindings

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -18,7 +18,8 @@ return {
     vim.keymap.set("n", "<leader>ah", function()
       require("dapui").toggle()
     end, { desc = "Flutter:DapUI Toggle" })
-    vim.keymap.set("n", "<leader>ar", "<cmd>FlutterLspRestart<cr>", { desc = "Flutter:Restart LSP" })
+    vim.keymap.set("n", "<leader>aR", "<cmd>FlutterLspRestart<cr>", { desc = "Flutter:Restart LSP" })
+    vim.keymap.set("n", "<leader>ar", "<cmd>FlutterReanalyze<cr>", { desc = "Flutter:Reanalyze LSP" })
 
     -- require("telescope").load_extension("flutter")
   end,
